9d090706d80c978bf6269346238812c5b8075db4
fahmi997 <aardiansyah.fahmii@gmail.com>
2023-10-14 19:44:30 +0700

    fix: addroute for login and timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import TimelinePage from './page/timeline'
 import AuthPage from './page/Auth'
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
@@ -9,8 +9,10 @@ function App() {
 
   return (
     <>
-      <AuthPage/>
-      <TimelinePage/>
+    <Routes>
+      <Route path="/" element={<AuthPage />} />
+      <Route path="/dashboard" element={<TimelinePage />} />
+    </Routes>
     </>
   )
 }
